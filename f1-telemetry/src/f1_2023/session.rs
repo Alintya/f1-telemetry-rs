3a60c5d0116586daedbf9458f9a9847845f53415
Alintya <Alintya@users.noreply.github.com>
2024-11-29 11:52:26 +0100

    fix: add SafetyCar::FormationLap
    apparently present since F1 2020

--- f1-telemetry/src/f1_2023/session.rs
+++ f1-telemetry/src/f1_2023/session.rs
@@ -83,6 +83,7 @@ fn unpack_safety_car(value: u8) -> Result<SafetyCar, UnpackError> {
         0 => Ok(SafetyCar::None),
         1 => Ok(SafetyCar::Full),
         2 => Ok(SafetyCar::Virtual),
+        3 => Ok(SafetyCar::FormationLap),
         _ => Err(UnpackError(format!("Invalid SafetyCar value: {}", value))),
     }
 }
